a4a76c5261bd241353f8d5f56e66a2bcfab4f922
lcf <kevinfinley@163.com>
2025-03-07 20:54:02 +0800

    (feat) Added filtering. Added workflow

--- src/labtasker_plugin_task_count/impl.py
+++ src/labtasker_plugin_task_count/impl.py
@@ -1,11 +1,13 @@
 from labtasker.client.core.api import ls_tasks
 
 
-def get_counts(limit=100):
+def get_counts(limit=100, extra_filter=None):
+    extra_filter = extra_filter or {}
     status = ["pending", "running", "success", "failed", "cancelled"]
     results = {}
     for s in status:
-        cnt = len(ls_tasks(extra_filter={"status": s}, limit=limit).content)
+        extra_filter["status"] = s
+        cnt = len(ls_tasks(extra_filter=extra_filter, limit=limit).content)
         if cnt >= limit:
             results[s] = f">= {limit}"
         else:
--- src/labtasker_plugin_task_count/main.py
+++ src/labtasker_plugin_task_count/main.py
@@ -1,8 +1,13 @@
-from labtasker.client.cli.task import app
-from labtasker.client.core.cli_utils import cli_utils_decorator
-from labtasker.client.core.logging import stdout_console
+from typing import Optional
 
+import typer
+
+from labtasker.client.cli.task import app
+from labtasker.client.core.cli_utils import cli_utils_decorator, parse_metadata
+from labtasker.client.core.logging import stdout_console
 from .impl import get_counts
+
+# For a simpler version, you may refer to 'simple' git branch
 
 # To customize command, you simply register your cmd function using Typer app
 # For example, if you wish to implement a sub-command of "task", i.e. labtasker task my-command
@@ -14,9 +19,19 @@ from .impl import get_counts
 
 @app.command()
 @cli_utils_decorator
-def count(limit: int = 100):
+def count(
+    limit: int = 100,
+    extra_filter: Optional[str] = typer.Option(
+        None,
+        "--extra-filter",
+        "-f",
+        help='Optional mongodb filter as a dict string (e.g., \'{"$and": [{"metadata.tag": {"$in": ["a", "b"]}}, {"priority": 10}]}\').',
+    ),
+):
     """Give a brief summary of the numbers of tasks in each status."""
-    result = get_counts(limit=limit)
+    extra_filter = parse_metadata(extra_filter)
+
+    result = get_counts(limit=limit, extra_filter=extra_filter)
 
     fmt_status = {
         "pending": "🟡 Pending",
